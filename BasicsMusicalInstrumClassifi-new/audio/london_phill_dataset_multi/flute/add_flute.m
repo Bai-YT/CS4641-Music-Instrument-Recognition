101c703c3912dcba66103c652f18d27bf4cf84a5
Bai-YT <Cu29Ar3d10@gmail.com>
2020-07-13 13:40:34 -0400

    Modified files and datasets

---
+++ BasicsMusicalInstrumClassifi-new/audio/london_phill_dataset_multi/flute/add_flute.m
@@ -0,0 +1,19 @@
+myFiles = dir(fullfile(pwd,'*.mp3')); %gets all wav files in struct
+
+for i=1:length(myFiles)
+    [y1, Fs1] = audioread(myFiles(i).name);
+    for j=1:4
+        k = ceil(rand*length(myFiles));
+        while(k == i)
+            k = ceil(rand*length(myFiles));
+        end
+        [y2, Fs2] = audioread(myFiles(k).name);
+        if Fs1 ~= Fs2
+            continue;
+        end
+        len = min(length(y1), length(y2));
+        audiowrite(sprintf('combined/flute_%d_%d.wav', i, k), (y1(1:len)+y2(1:len))/2, Fs1);
+    end
+end
+
+fprintf("Done.\n")
